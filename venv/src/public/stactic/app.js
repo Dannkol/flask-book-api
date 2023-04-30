a23b7bdefad6ff2a3ee66b4898d2bbb23b8b45d3
Daniel Manosalva <Manosalvasierra@gmail.com>
2023-04-30 02:54:42 +0200

    feat: :zap: scan qr
    prueba de scan de qr para login con libreria de instascan.js

--- venv/src/public/stactic/app.js
+++ venv/src/public/stactic/app.js
@@ -1,3 +1,15 @@
 import medicamentos from "./components/medicamentos.js";
-medicamentos.printMedicamentos();
-medicamentos.botonesss();
+
+
+
+document.addEventListener("DOMContentLoaded", (e)=>{
+    medicamentos.printMedicamentos();
+    medicamentos.botonesss();
+    const qr_scanner = document.querySelector("#qr_reader")
+    document.querySelector('#qr_boton').addEventListener('click', (e)=>{
+        document.querySelector('.modal-body').children[1].textContent = '';
+        qr_scanner.style.display = 'block'
+    })
+
+
+})
